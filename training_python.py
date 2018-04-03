39ea479677b4fb33ac36809cc66d8b327e292ce3
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-04-03 09:13:38 -0700

    update

--- training_python.py
+++ training_python.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 """ Sorts Algorithms and Convexity """
 
@@ -39,8 +40,25 @@ def merge(left, right):
 
 
 
-def quicksort(x):
-    pass
+def quicksort(x, low, high):
+    if low < high:
+        partition = create_partition(x, low, high)
+        quicksort(x, low, partition-1)
+        quicksort(x, partition+1, high)
+    else:
+        return
+
+def create_partition(x, low, high):
+    pivot = low
+    for i in range(low+1, high+1):
+        if x[i] <= x[low]:
+            pivot += 1
+            x[i], x[pivot] = x[pivot], x[i]
+    x[pivot], x[low] = x[low], x[pivot]
+    return pivot
+
+
+
 
 
 def bubble_sort(x):
@@ -49,4 +67,13 @@ def bubble_sort(x):
 
 if __name__=='__main__':
     x = [3, 5, 7, 1, 8, 2, 8, 5, 8, 1]
-    print(merge_sort(x))
+    start_time = time.time()
+    x = merge_sort(x)
+    end_time = time.time() - start_time
+    print('sorted list with merge sort: %s in : %s s' % (x, end_time))
+
+    y = [3, 5, 7, 1, 8, 2, 8, 5, 8, 1]
+    start_time = time.time()
+    quicksort(y, 0, len(x)-1)
+    end_time = time.time() - start_time
+    print('sorted list with quicksort: %s in : %s s' % (y, end_time))
