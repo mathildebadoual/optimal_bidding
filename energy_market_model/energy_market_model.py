af864cb968d5738a394f1d180d0a4370d64db5cc
Mathilde Badoual <mathilde.badoual@gmail.com>
2018-10-15 11:22:10 -0700

    start RL project

---
+++ energy_market_model/energy_market_model.py
@@ -0,0 +1,24 @@
+### Energy Market Model ###
+
+import numpy as np
+import cvxpy as cvx
+import pandas as pd
+
+class MarketModel():
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self):
+        pass
+
+
+
+class Generator(self):
+    def __init__(self):
+        pass
+
+
+class
