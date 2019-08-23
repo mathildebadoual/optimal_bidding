23165ba90b13ba5cde3f56ce250d8a6760604418
JPalakapilly <jjp126@gmail.com>
2019-08-23 15:23:57 -0700

    function creates multiple transition matrices instead of just one per day.

--- optimal_bidding/environments/utils.py
+++ optimal_bidding/environments/utils.py
@@ -144,30 +144,41 @@ def round_to_nearest(x, base):
     # helper function to bin values
     return base * np.round(x/base)
 
-def get_transition_probabilities(data, bin_size=10):
+def get_transition_probabilities(df, column="Price", bin_size=10, timestep=30):
     """
-    First discretizes data by rounding the values to the nearest bin_size.
+    First discretizes the data by rounding the values to the nearest bin_size.
     Then, calculates transition probabilites from consecutive rounded values in data.
+    Calculates separate transition probabilities for each timestep. 
 
     Parameters
     ----------
-    data: 
-        Name of the column in df that contains the data.
+    df: dataframe
+        Dataframe holding the data to be analyzed
+    column: size
+        The name of the column in df that holds the data. 
     bin_size: int
         Values in in data will be rounded to the nearest bin_size.
-
+    timestep: int
+        The number of minutes in each timestep.
+    
     Returns
     -------
-    probs: dict
-        A nested dictionary.
-        The keys of the outer dictionary are the "before" states.
-        The values of the outer dictionary are also dictionaries (inner dictionaries).
-        The keys of the inner dictionaries are "after" states.
-        The values of the inner dictionaries are transition probabilities.
+    all_probs: list
+        A list of nested dictionaries.
+        The index in this list corresponds to the time of day. An element with index n contains the
+        transition probabilities for the timestep that starts at n*timestep minutes after 0:00.
+        Each element is structured as:
+            The keys of the outer dictionary are the "before" states.
+            The values of the outer dictionary are also dictionaries (inner dictionaries).
+            The keys of the inner dictionaries are "after" states.
+            The values of the inner dictionaries are transition probabilities.
     """
     data = df[column].values
-    counts = {}
+    times = df.index
+    all_counts = [dict() for x in range(int(24*60/timestep))]
     for i in range(len(data) - 1):
+        matrix_index = int(times[i].hour + times[i].minute / timestep)
+        counts = all_counts[matrix_index]
         current_state = data[i]
         next_state = data[i+1]
         if current_state in counts:
@@ -180,13 +191,17 @@ def get_transition_probabilities(data, bin_size=10):
         else:
             counts[current_state] = {next_state:1, "total": 1}
             
-    probs = {}
-    for state in counts:
-        temp = counts[state]
-        total_count = temp["total"]
-        transitions = {}
-        for next_state in temp:
-            if next_state != "total":
-                transitions[next_state] = temp[next_state] / total_count
-        probs[state] = transitions
-    return probs
+    all_probs = [dict() for x in range(int(24*60/timestep))]
+    for i in range(len(all_counts)):
+        probs = all_probs[i]
+        counts = all_counts[i]
+        for state in counts:
+            temp = counts[state]
+            total_count = temp["total"]
+            transitions = {}
+            for next_state in temp:
+                if next_state != "total":
+                    transitions[next_state] = temp[next_state] / total_count
+            probs[state] = transitions
+    return all_probs
+
