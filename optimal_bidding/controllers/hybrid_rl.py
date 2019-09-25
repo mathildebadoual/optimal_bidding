4db77e987ee27dd33976594430729ee82d5df217
Mathilde Badoual <mathilde.badoual@gmail.com>
2019-09-25 14:58:22 -0700

    trying :(

--- optimal_bidding/controllers/hybrid_rl.py
+++ optimal_bidding/controllers/hybrid_rl.py
@@ -8,7 +8,7 @@ sys.path.append(
         os.path.dirname(os.path.dirname(os.path.realpath(__file__)))))
 from optimal_bidding.environments.energy_market import FCASMarket
 from optimal_bidding.environments.agents import Battery, Bid
-from optimal_bidding.utils.nets import ActorNet, CriticNet
+from optimal_bidding.utils.nets import ActorNet, CriticNet, Network, Layer
 import optimal_bidding.utils.data_postprocess as data_utils
 import torch
 
@@ -16,7 +16,7 @@ import torch
 class ActorCritic():
     def __init__(self):
         # hyperparameters
-        self._exploration_size = None
+        self._sig = 0.1
 
         self._actor_step_size = 0.1
         self._critic_step_size = 0.01
@@ -26,10 +26,34 @@ class ActorCritic():
         torch.manual_seed(0)
         self._fcas_market = FCASMarket()
         self._battery = Battery()
-        self._actor_nn = ActorNet()
-        self._critic_nn = CriticNet()
 
-        self._e = [0] * len(list(self._critic_nn.parameters()))
+        l1w = np.random.normal(0, self._sig, (20, 6))
+        l1b = np.random.normal(0, self._sig, 20)
+
+        l2w = np.random.normal(0, self._sig, (10, 20))
+        l2b = np.random.normal(0, self._sig, 10)
+
+        l3w = np.random.normal(0, self._sig, (8, 10))
+        l3b = np.random.normal(0, self._sig, 8)
+
+        l4w = np.random.normal(0, self._sig, (1, 8))
+        l4b = np.random.normal(0, self._sig, 1)
+
+        l5w = np.random.normal(0, self._sig, (3, 8))
+        l5b = np.random.normal(0, self._sig, 3)
+
+        self._actor_nn = Network(
+            [Layer(l1w, l1b, False),
+             Layer(l2w, l2b),
+             Layer(l3w, l3b),
+             Layer(l5w, l5b, False)])
+        self._critic_nn = Network(
+            [Layer(l1w, l1b, False),
+             Layer(l2w, l2b),
+             Layer(l3w, l3b),
+             Layer(l4w, l4b, False)])
+
+        self._e = [0] * len(self._critic_nn.layers)
         self._delta = None
 
     def run_simulation(self):
@@ -55,12 +79,10 @@ class ActorCritic():
             energy_cleared_price = data_utils.get_energy_price(timestamp)
             prev_fcas_clearing_price = fcas_clearing_price
             raise_demand = data_utils.get_raise_demand(timestamp)
-            state = torch.tensor(
-                np.array([
-                    step_of_day, soe, prev_energy_cleared_price,
-                    energy_cleared_price, prev_fcas_clearing_price,
-                    raise_demand
-                ]))
+            state = np.array([
+                step_of_day, soe, prev_energy_cleared_price,
+                energy_cleared_price, prev_fcas_clearing_price, raise_demand
+            ])
 
             # compute the action = [p_raise, c_raise, p_energy]
             action_supervisor, action_actor, action_exploration, action_composite = self._compute_action(
@@ -70,7 +92,8 @@ class ActorCritic():
             bid_fcas, bid_energy = self._transform_to_bid(
                 action_composite, energy_cleared_price)
 
-            bid_fcas, bid_energy, reward_shield = self._shield(bid_fcas, bid_energy)
+            bid_fcas, bid_energy, reward_shield = self._shield(
+                bid_fcas, bid_energy, action_supervisor)
 
             print('bid power: %s' % bid_fcas.power_signed())
             print('bid price: %s' % bid_fcas.price())
@@ -94,80 +117,57 @@ class ActorCritic():
             next_step_of_day = self._get_step_of_day(next_timestamp)
             next_energy_cleared_price = data_utils.get_energy_price(next_timestamp)
             next_raise_demand = data_utils.get_raise_demand(next_timestamp)
-            next_state = torch.tensor([
+            next_state = np.array([
                 next_step_of_day, next_soe, energy_cleared_price,
                 next_energy_cleared_price, fcas_clearing_price,
                 next_raise_demand
             ])
 
-            print(state)
-            print(next_state)
+            print(action_actor)
 
             # update eligibility and delta
-            current_state_value = self._critic_nn(state.float())
-            next_state_value = self._critic_nn(next_state.float())
+            current_state_value = self._critic_nn.compute_value(state)
+            next_state_value = self._critic_nn.compute_value(next_state)
             self._delta = reward + self._discount_factor * next_state_value -\
                     current_state_value
 
-            action_actor_numpy = action_actor.data.numpy()
-            bid_fcas_actor_power = abs(action_actor_numpy[0])
-            bid_fcas_actor_price = action_actor_numpy[1]
-            bid_energy_actor_power = action_actor_numpy[2]
+            bid_fcas_actor_power = abs(action_actor[0])
+            bid_fcas_actor_price = action_actor[1]
+            bid_energy_actor_power = action_actor[2]
 
             # update neural nets
-            self._update_critic(current_state_value)
-            self._update_actor(action_supervisor, action_actor,
-                               action_exploration, k)
+            self._update_critic(state)
+            self._update_actor(action_supervisor, action_actor, action_exploration, k, state)
 
             save_data(bid_fcas, bid_energy, bid_fcas_cleared,
                       fcas_clearing_price, soe, index, timestamp,
                       energy_cleared_price, reward, current_state_value,
-                      next_state_value, raise_demand,
-                      bid_fcas_actor_power, bid_fcas_actor_price,
-                      bid_energy_actor_power, k, self._delta)
+                      next_state_value, raise_demand, bid_fcas_actor_power,
+                      bid_fcas_actor_price, bid_energy_actor_power, k,
+                      self._delta)
 
             index += 1
-            if index % 100 ==0 and k < 1:
+            if index % 100 == 0 and k < 1:
                 k += 0.1
                 print(k)
 
-    def _update_critic(self, current_state_value):
-        current_state_value.backward()
-        for i, f in enumerate(self._critic_nn.parameters()):
+    def _update_critic(self, state):
+        grad_critic = self._critic_nn.compute_value_and_derivative_w(state)
+        for i, l in enumerate(self._critic_nn.layers):
             self._e[i] = self._discount_factor * self._e_tdf * self._e[
-                i] + f.grad.data
-            f.data.sub(- self._critic_step_size * self._delta * self._e[i])
-        self._critic_nn.zero_grad()
+                i] + grad_critic[i]
+            l.weights_matrix += -self._critic_step_size * self._delta * self._e[
+                i]
 
     def _update_actor(self, action_supervisor, action_actor,
-                      action_exploration, k):
-        grad_input_vectors = [
-            torch.tensor([1., 0, 0]),
-            torch.tensor([0, 1., 0]),
-            torch.tensor([0, 0, 1.])
-        ]
-        grads = []
-
-        for i, v in enumerate(grad_input_vectors):
-            temp = []
-            if i != 2:
-                action_actor.backward(v, retain_graph=True)
-            else:
-                action_actor.backward(v, retain_graph=False)
-            for f in self._actor_nn.parameters():
-                temp.append(f.grad.data.clone())
-                self._actor_nn.zero_grad()
-            grads.append(temp)
-
-        action_vector = (k * self._delta * action_exploration + (1 - k) * (action_supervisor - action_actor))[0]
-
-        for i, f in enumerate(self._actor_nn.parameters()):
-            for j in range(3):
-                f.data.sub(self._actor_step_size * action_vector[j] *
-                           grads[j][i])
+                      action_exploration, k, state):
+        action_vector = (k * self._delta * action_exploration + (1 - k) *
+                         (action_supervisor - action_actor))[0]
+        grad_action = self._actor_nn.compute_value_and_derivative_w(state)
+        update = self._actor_step_size * np.dot(action_vector, grad_action)
 
     def _transform_to_bid(self, action, energy_cleared_price):
-        action = action[0].data.numpy()
+        action = action[0]
         bid_fcas = Bid(-action[0], action[1], bid_type='gen')
         if action[2] >= 0:
             bid_energy = Bid(action[2], energy_cleared_price, bid_type='load')
@@ -185,13 +185,13 @@ class ActorCritic():
     def _compute_action(self, state, timestamp, k):
         # timestamp = datetime.fromtimestamp(timestamp)
         bid_fcas_mpc, bid_energy_mpc = self._battery.bid_mpc(timestamp)
-        action_supervisor = torch.tensor([
+        action_supervisor = np.array([
             bid_fcas_mpc.power_signed(),
             bid_fcas_mpc.price(),
             bid_energy_mpc.power_signed()
         ])
-        action_actor = self._actor_nn(state.float())
-        action_exploration = torch.randn(1, 3)
+        action_actor = self._actor_nn.compute_value(state)
+        action_exploration = np.random.normal(0, self._sig, (1, 3))
         return action_supervisor, action_actor, action_exploration, (1 - k) * action_supervisor + k  * (action_actor + action_exploration)
 
     def _compute_reward(self, bid_fcas, bid_energy, energy_cleared_price,
--- optimal_bidding/utils/nets.py
+++ optimal_bidding/utils/nets.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+
 
 class ActorNet(nn.Module):
 
@@ -58,3 +60,95 @@ class CriticNet(nn.Module):
         x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+
+
+class Layer:
+    def __init__(self, weights_matrix, bias_vector, sigmoid_activation=True):
+        self.weights_matrix = weights_matrix
+        self.bias_vector = bias_vector
+        self.sigmoid_activation = sigmoid_activation
+
+    def compute_value(self, x_vector):
+        result = np.add(np.dot(self.weights_matrix, x_vector),
+                        self.bias_vector)
+        if self.sigmoid_activation:
+            result = np.exp(-result)
+            result = 1 / (1 + result)
+        return result
+
+    def compute_value_without_activation(self, x_vector):
+        result = np.add(np.dot(self.weights_matrix, x_vector),
+                        self.bias_vector.reshape((-1, 1)))
+        return result
+
+    def compute_value_and_derivative(self, x_vector):
+        if not self.sigmoid_activation:
+            return (self.compute_value(x_vector), self.weights_matrix)
+        temp = np.add(np.dot(self.weights_matrix, x_vector), self.bias_vector)
+        temp = np.exp(-temp)
+        value = 1.0 / (1 + temp)
+        temp = temp / (1 + temp)**2
+        #pre-multiplying by a diagonal matrix multiplies each row by
+        #the corresponding diagonal element
+        #(1st row with 1st value, 2nd row with 2nd value, etc...)
+        jacobian = np.dot(np.diag(temp), self.weights_matrix)
+        return (value, jacobian)
+
+
+class Network:
+    def __init__(self, layers):
+        self.layers = layers
+
+    def compute_value(self, x_vector):
+        for l in self.layers:
+            x_vector = l.compute_value(x_vector)
+        return x_vector
+
+    def compute_value_and_derivative(self, x_vector):
+        x_vector, jacobian = self.layers[0].compute_value_and_derivative(
+            x_vector)
+        for l in self.layers[1:]:
+            x_vector, j = l.compute_value_and_derivative(x_vector)
+            jacobian = np.dot(j, jacobian)
+        return x_vector, jacobian
+
+    def compute_value_and_derivative_w(self, x_vector):
+        list_vector_grad = []
+        list_vector = []
+        x_vector_ = x_vector.copy()
+        list_vector.append(x_vector_.reshape((-1, 1)))
+        for l in self.layers:
+            x_vector_ = l.compute_value(x_vector_)
+            list_vector.append(x_vector_.reshape((-1, 1)))
+        print([a.shape for a in list_vector])
+        n = len(list_vector)
+        prev_l = None
+        prev_grad = None
+        for i, l in enumerate(reversed(self.layers)):
+            print(i)
+            d_sig = d_sigmoid(
+                l.compute_value_without_activation(list_vector[n - i - 2]))
+            list_grad_layer = []
+            if i == 0:
+                prev_grad = d_sig
+                print(prev_grad.shape)
+                for j in range(l.weights_matrix.shape[0]):
+                    list_grad_layer.append(
+                        np.dot(prev_grad[i], list_vector[n - i - 2].T))
+                list_vector_grad.append(list_grad_layer)
+            else:
+                print(prev_grad.dot(prev_l.weights_matrix).shape)
+                print(d_sig.shape)
+                prev_grad = prev_grad.dot(prev_l.weights_matrix) * d_sig
+                print(prev_grad.shape)
+                for j in range(l.weights_matrix.shape[0]):
+                    list_grad_layer.append(
+                        np.dot(prev_grad[j].reshape((-1, 1)), list_vector[n - i - 2].T))
+                list_vector_grad.append(list_grad_layer)
+            prev_l = l
+        return [g for g in reversed(list_vector_grad)]
+
+
+def d_sigmoid(x):
+    x = np.exp(-x)
+    return x / (1 + x)**2
